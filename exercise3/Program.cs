ae26e66d8d9301dbe924b5028b6cc6b564f4e20e
ismail <511is@mail.ru>
2022-08-15 20:31:30 +0300

    ex4

--- exercise3/Program.cs
+++ exercise3/Program.cs
@@ -1,23 +1,6 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-
-Console.WriteLine("Введите число>");
-int a = Convert.ToInt32(Console.ReadLine());
-int s = 3;
-int a1 = a /10 % 10;
-Console.Write(a1);
-
-
-
-
-
 Random rnd = new Random();
-int x = rnd.Next(100 , 999);
-int s = 2;
-int m = 1;
-while (s > 1) {
-     m *= 10;
-     s--;
-}
-int t = (x % m) + (x / (m * 10)) * m;
-Console.WriteLine(t);
+int a = rnd.Next(100 , 1000);
+Console.WriteLine(a);
+int b = 10 * ( a / 100 ) + ( a % 10 );
+Console.Write(b);
---
+++ exercise4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("Введите день недели>");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A > 5) 
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это будний день");
+}
