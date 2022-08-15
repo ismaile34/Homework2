64c630f53d3b5af750adfdf353fa3de4d6080719
ismaile34 <47721975+ismaile34@users.noreply.github.com>
2022-08-15 19:28:11 +0300

    Update Program.cs

--- exercise3/Program.cs
+++ exercise3/Program.cs
@@ -1,18 +1,7 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-
-Console.WriteLine("Введите число>");
-int a = Convert.ToInt32(Console.ReadLine());
-int s = 3;
-int a1 = a /10 % 10;
-Console.Write(a1);
-
-
-
-
-
 Random rnd = new Random();
 int x = rnd.Next(100 , 999);
+Console.WriteLine(x);
 int s = 2;
 int m = 1;
 while (s > 1) {
