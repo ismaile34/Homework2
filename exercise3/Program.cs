52f7e9c8d79ae20ff8f900803305c3640c412f04
ismail <511is@mail.ru>
2022-08-15 17:06:57 +0300

    Homework2

---
+++ exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите число>");
+int a = Convert.ToInt32(Console.ReadLine());
+int s = 3;
+int a1 = a /10 % 10;
+Console.Write(a1);
+
+
+
+
+
+Random rnd = new Random();
+int x = rnd.Next(100 , 999);
+int s = 2;
+int m = 1;
+while (s > 1) {
+     m *= 10;
+     s--;
+}
+int t = (x % m) + (x / (m * 10)) * m;
+Console.WriteLine(t);
