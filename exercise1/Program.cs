4af6dee71f065f81418a6fe74d846fb5bd2e06e3
ismail <511is@mail.ru>
2022-08-15 17:14:32 +0300

    fix

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -1,9 +1,4 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int Prompt(string message);
-{
-    System.Console.Write(message);
-    
-}
 Console.WriteLine("Введите трехзначное число>");
 int A = Convert.ToInt32(Console.ReadLine());
 int A1 = A /10 % 10;
